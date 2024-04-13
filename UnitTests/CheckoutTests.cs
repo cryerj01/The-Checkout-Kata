d67b1c5f94a0ef0087d1be98927f57fbf2a2b65a
Cryerj01 <turne@DESKTOP-QUG8IJR>
2024-04-13 16:17:57 +0100

    made more tests, made tests description to reflect saning mulit products at a time or scaning one at a time.

--- UnitTests/CheckoutTests.cs
+++ UnitTests/CheckoutTests.cs
@@ -1,3 +1,4 @@
+using Microsoft.VisualBasic;
 using System.ComponentModel.Design;
 using The_Checkout_kata;
 using The_Checkout_Kata;
@@ -5,51 +6,60 @@ using The_Checkout_Kata;
 namespace CheckoutTests
 {
     [TestClass]
-    public class CheckoutTestsPrice
+    public class CheckoutTestsPricebulkScan
     {
+        public Checkout checkout = new Checkout();
+
         [TestMethod]
 
         [DataRow(50, "A")]
         [DataRow(80, "AB")]
         [DataRow(100, "ABC")]
         [DataRow(115,"ABCD")]
-        //random order
         [DataRow(80, "BA")]
         [DataRow(100, "BAC")]
         [DataRow(115, "ADCB")]
 
         public void AllNumberAddUpWithNoSpecialPrice(int total,string products)
         {
-     
-            var checkout = new Checkout();
+            
             checkout.Scan(products);
-                      
-          Assert.AreEqual(total,checkout.total);
-
-
+            Assert.AreEqual(total,checkout.total);
         }
+
+
+
 
         [TestMethod]
         [DataRow(130, "AAA")]
         [DataRow(260, "AAAAAA")]
         [DataRow(160,"AAAB")]
+        [DataRow(175, "ABAAB")]
+        [DataRow(175, "ABABA")]
+
         public void PricesWithSpecials(int total, string products) {
-            var checkout = new Checkout();
             checkout.Scan(products);
-
             Assert.AreEqual(total, checkout.total);
-
-
         }
 
-   
+        [TestMethod]
+        [DataRow(160, "ABAA")]
+        [DataRow(165,"ABCAB")]
+
+        public void MixOfSpecialAndNoneSpecial(int total, string products) {
+            checkout.Scan(products);
+            Assert.AreEqual(total, checkout.total);
+        }
 
 
 
     }
 
     [TestClass]
-    public class CheckoutTestsSavings {
+    public class CheckoutTestsSavingsbulkscan {
+        public Checkout checkout = new Checkout();
+
+
         [TestMethod]
         [DataRow(20, "AAA")]
         [DataRow(15, "BB")]
@@ -59,13 +69,45 @@ namespace CheckoutTests
         [DataRow(0, "ABA")]
 
         public void checkSavings(int savings, string products)
-        {
-
-            var checkout = new Checkout();
+        { 
             checkout.Scan(products);
-
             Assert.AreEqual(savings, checkout.saving);
-
         }
+        [TestMethod]
+        [DataRow("A")]
+        [DataRow("AB")]
+        [DataRow("ABC")]
+        [DataRow("ABCD")]
+        //random order
+        [DataRow( "BA")]
+        [DataRow("BAC")]
+        [DataRow("ADCB")]
+        public void checkNoSpecialsSaving(string products) {
+            checkout.Scan(products);
+            Assert.AreEqual(0, checkout.saving);
+        }
+    }
+
+    [TestClass]
+    public class CheckoutTestsPriceMultiScan
+    {
+        Checkout checkout = new Checkout();
+        [TestMethod]
+        [DataRow(50, "A")]
+        [DataRow(80, "AB")]
+        [DataRow(100, "ABC")]
+        [DataRow(115, "ABCD")]
+        [DataRow(80, "BA")]
+        [DataRow(100, "BAC")]
+        [DataRow(115, "ADCB")]
+
+        public void checkPriceMultiScan(int total,string products) {
+        foreach (char p in products.ToCharArray()) {
+                checkout.Scan(p.ToString());
+            }
+        Assert.AreEqual(total, checkout.total);
+        }
+
+
     }
 }
